fe155d17da40b1d198d700ce7ba143c253b4619d
ysfsamir13 <ysfsamir13@gmail.com>
2026-03-08 03:03:28 +0100

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './database.types'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
 export const supabase = createClient<Database>(
-  supabaseUrl ?? '',
-  supabaseAnonKey ?? ''
+  'https://ylqehynxhlcoebepfsoy.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlscWVoeW54aGxjb2ViZXBmc295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjI4NDMsImV4cCI6MjA4ODM5ODg0M30.SmD1JkpqX1tMUILhEqLu7YqsLB9_frVWfQuzQtXXTWc'
 )
